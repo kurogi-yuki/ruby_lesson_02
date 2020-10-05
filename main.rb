be9a9163005ae408ce225faf1d008f6200cdaf51
kurogi yuki <xtoriymoonz2@yahoo.co.jp>
2020-10-05 15:50:21 +0900

    addインスタンス変数

--- main.rb
+++ main.rb
@@ -5,12 +5,12 @@ require_relative "user"
 plans = Plan.import(path: "plans.csv")
 user = User.new
 
-puts "旅行プランを選択してください"
+puts "旅行プランを選択してください。"
 
 plans.each do |plan|
   puts plan.index
 end
 
-number = user.number(plans)
-member = user.member_sum(number)
-user.pay_sum(number, member)
+user.number(plans)
+user.member_sum
+user.pay_sum
--- user.rb
+++ user.rb
@@ -4,35 +4,34 @@ class User
     plan_number = gets.chomp.to_i
 
     while plan_number > plans.size || plan_number <= 0
-      puts "1〜#{plans.size}の間で入力し直してください"
+      puts "1〜#{plans.size}の間で入力し直してください。"
       plan_number = gets.chomp.to_i
     end
 
-    plan_choice = plans[plan_number - 1]
+    @plan = plans[plan_number - 1]
 
-    return plan_choice
   end
 
-  def member_sum(number)
-    puts "#{number.place}ですね、何人で行きますか？"
+  def member_sum
+    puts "#{@plan.place}ですね。何人で行きますか？"
     member = gets.chomp.to_i
 
     while member <= 0
-      puts "人数は0人以上にしてください"
+      puts "人数は0人以上にしてください。"
       member = gets.chomp.to_i
     end
 
-    return member
+    @member = member
   end
 
-  def pay_sum(number, member)
-    puts "#{member}人ですね"
+  def pay_sum
+    puts "#{@member}人ですね。"
 
-    if member >= 5
-      puts "五人以上なので10%割引となります"
-      total_price = (number.price * member) * 90/100
+    if @member >= 5
+      puts "五人以上なので10%割引となります。"
+      total_price = (@plan.price * @member) * 90/100
     else
-      total_price = number.price * member
+      total_price = @plan.price * @member
      end
 
      puts "合計金額: #{total_price}円"
